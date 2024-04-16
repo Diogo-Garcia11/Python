0836068363fb8d70880502487bf703c24750f789
Diogo-Garcia11 <142684349+Diogo-Garcia11@users.noreply.github.com>
2024-04-15 22:43:38 -0300

    giu

---
+++ tipos_de_dados.py
@@ -0,0 +1,12 @@
+print( 11 + 10 + 1000)
+print( 1.5 + 2.5 + 1 + 0.5)
+print(True)
+print(False)
+
+
+print("Python" + 'é' + """legal""" + '''!''')
+diogo = 11
+diogo2= 12
+print( diogo + diogo2)
+
+print (f'diogo + diogo2 = {diogo} {diogo2}')
