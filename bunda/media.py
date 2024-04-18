cb292ba7661a4f0450eb2a6eb56e0c64879366c6
Diogo Garcia <142684349+Diogo-Garcia11@users.noreply.github.com>
2024-04-18 16:23:14 -0300

    Add files via upload

---
+++ bunda/media.py
@@ -0,0 +1,14 @@
+def media(Nota1,Nota2,Nota3):
+    #{
+    Media = (int(Nota1) + int(Nota2) + int(Nota3)) /3
+    if Media < 4:
+        #
+        if Nota1 == 5:
+            #
+            print("boa")
+            #
+        #  
+    elif Media >= 5:
+        print("bungas")
+    return Media
+    #}
