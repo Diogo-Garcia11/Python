d7b550e0b6a87a2c083f5bc0b17bce159220f48d
Diogo-Garcia11 <diogo.garcia3@etec.sp.gov.br>
2024-06-27 22:29:42 -0300

    rollback adicionado mto importante

--- proj_mysql/conexao.py
+++ proj_mysql/conexao.py
@@ -31,54 +31,78 @@ class Conexao:
 
     def Inserir(primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento):
         # """Insere um novo usuário na tabela."""
-        CONEXAO = Conexao.Dados()
-        CURSOR = CONEXAO.cursor()
-        CURSOR.execute("""
-            INSERT INTO tb_usuario (primeiro_nome_usuario, ultimo_nome_usuario, email_usuario, senha_usuario, cpf_usuario, data_nasc_usuario) VALUES (%s, %s, %s, %s, %s, %s)
-        """, (primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento))
-        CONEXAO.commit()
-        CURSOR.close()
-        print("Dados Cadastrados com sucesso no banco de dados.")
+        try:
+            CONEXAO = Conexao.Dados()
+            CURSOR = CONEXAO.cursor()
+            CURSOR.execute("""
+                INSERT INTO tb_usuario (primeiro_nome_usuario, ultimo_nome_usuario, email_usuario, senha_usuario, cpf_usuario, data_nasc_usuario) VALUES (%s, %s, %s, %s, %s, %s)
+            """, (primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento))
+            CONEXAO.commit()
+            print("Dados Cadastrados com sucesso no banco de dados.")
+        except Exception as e:
+            print(f"Erro ao inserir: {e}")
+            CONEXAO.rollback()
+        finally:
+            CURSOR.close()
+            
 
     def Atualizar(primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento):
-        CONEXAO = Conexao.Dados()
-        CURSOR = CONEXAO.cursor()
-        CURSOR.execute("""
-            UPDATE tb_usuario SET primeiro_nome_usuario = %s, ultimo_nome_usuario = %s, email_usuario = %s, senha_usuario = %s, cpf_usuario = %s, data_nasc_usuario = %s WHERE cpf_usuario = %s
-        """, (primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento, cpf))
-        CONEXAO.commit()
-        CURSOR.close()
-        print("Dados atualizados com sucesso no banco de dados.")
-
+        try:
+            CONEXAO = Conexao.Dados()
+            CURSOR = CONEXAO.cursor()
+            CURSOR.execute("""
+                UPDATE tb_usuario SET primeiro_nome_usuario = %s, ultimo_nome_usuario = %s, email_usuario = %s, senha_usuario = %s, cpf_usuario = %s, data_nasc_usuario = %s WHERE cpf_usuario = %s
+            """, (primeiro_nome, ultimo_nome, email, senha, cpf, data_nascimento, cpf))
+            CONEXAO.commit()
+            print("Dados atualizados com sucesso no banco de dados.")
+        except Exception as e:
+            print(f"Erro ao atualizar: {e}")
+            CONEXAO.rollback()
+        finally:
+            CURSOR.close()
+        
     def ConsultaPersonalizada(email_usuario):
-        CONEXAO = Conexao.Dados()
-        CURSOR = CONEXAO.cursor()
-        CURSOR.execute("""
-            SELECT * FROM tb_usuario WHERE email_usuario = %s
-        """, (email_usuario,))
-        resultado = CURSOR.fetchall()
-        CURSOR.close()
-        resultado_formatado = [", ".join(map(str, Linha)) for Linha in resultado]
-        return resultado_formatado
+        try:
+            CONEXAO = Conexao.Dados()
+            CURSOR = CONEXAO.cursor()
+            CURSOR.execute("""
+                SELECT * FROM tb_usuario WHERE email_usuario = %s
+            """, (email_usuario,))
+            resultado = CURSOR.fetchall()
+            resultado_formatado = [", ".join(map(str, Linha)) for Linha in resultado]
+            return resultado_formatado
+        except Exception as e:
+            print(f"Erro ao consultar: {e}")
+        finally:
+            CURSOR.close()
     
     def ConsultaGeral():
-        CONEXAO = Conexao.Dados()
-        CURSOR = CONEXAO.cursor()
-        CURSOR.execute("""
-            SELECT * FROM tb_usuario
-        """)
-        resultado = CURSOR.fetchall()
-        CURSOR.close()
-        resultado_formatado = [", ".join(map(str, Linha)) for Linha in resultado]
-        return resultado_formatado
-    
+        try:
+            CONEXAO = Conexao.Dados()
+            CURSOR = CONEXAO.cursor()
+            CURSOR.execute("""
+                SELECT * FROM tb_usuario
+            """)
+            resultado = CURSOR.fetchall()
+            CURSOR.close()
+            resultado_formatado = [", ".join(map(str, Linha)) for Linha in resultado]
+            return resultado_formatado
+        except Exception as e:
+            print(f"Erro ao consultar: {e}")
+        finally:
+            CURSOR.close()
+        
     def Deletar(email_usuario):
-        CONEXAO = Conexao.Dados()
-        CURSOR = CONEXAO.cursor()
-        CURSOR.execute("""
-            DELETE FROM tb_usuario WHERE email_usuario =%s
-        """, (email_usuario,))
-        CONEXAO.commit()
-        CURSOR.close()
-        print("Dados Deletados com sucesso no banco de dados.")
-    
+        try:
+            CONEXAO = Conexao.Dados()
+            CURSOR = CONEXAO.cursor()
+            CURSOR.execute("""
+                DELETE FROM tb_usuario WHERE email_usuario =%s
+            """, (email_usuario,))
+            CONEXAO.commit()
+            print("Dados Deletados com sucesso no banco de dados.")
+        except Exception as e:
+            print(f"Erro ao deletar: {e}")
+            CONEXAO.rollback()
+        finally:
+            CURSOR.close()
